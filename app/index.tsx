105fec1286ef24c3b3c1516cdac00f4ba257fc87
LouiseDF <LD114160@hc.ac.je>
2026-03-19 11:30:21 +0000

    added a task component

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import { View } from 'react-native';
 import { Text } from '@/components/ui/text';
+import { Checkbox } from '@/components/ui/checkbox';
 
 export default function HomeScreen() {
-  return;
-  <View className="bg background flex flex-1 py-32">
-    <View className="flex flex-row">
-      <View className="items center flex w-16 justify-center"></View>
-      <Text className="text-foreground bg-blue-500">Submit Assignment</Text>
-      <Text className="text-foreground-transparent bg-orange-600">Due 20th Oct</Text>
+  // 1. Define your UseState here
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <View className="bg background flex flex-1 py-32">
+      <View className="flex flex-row">
+        <View className="items center flex w-16 justify-center"></View>
+        <Checkbox onCheckedChange={setChecked} checked={checked} className="border-2" />
+      </View>
+      <View className="border-foreground-transparent 1 flex border-b py-4"></View>
+      <Text className="text-foreground">Submit Assignment</Text>
+      <Text className="text-foreground-transparent">Due 20th Oct</Text>
     </View>
-  </View>;
+  );
 }
